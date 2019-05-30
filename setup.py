a7e9a5ad32fc158e8a90ca66d46209ff81f2c965
federico2001 <federicoriveroll@gmail.com>
2019-05-29 19:27:40 -0500

    Cambios

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'OpenBlender',         # How you named your package folder (MyLib)
+  packages = ['OpenBlender'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  description = 'OpenBlender API Service',   # Give a short description about your library
+  author = 'Federico Riveroll',                   # Type in your name
+  author_email = 'federico@openblender.io',      # Type in your E-Mail
+  url = 'https://www.openblender.io',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['OpenBlender'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
+          'pandas',
+          'datetime',
+          'requests'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
