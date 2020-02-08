53b0ebb88b616d8ebe30a78f366ce0e59158f160
federico2001 <federicoriveroll@gmail.com>
2020-02-07 19:28:06 -0600

    Detail update

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(
           'numpy',
           'pandas',
           'datetime',
-          'requests',
-          'base64'
+          'requests'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
