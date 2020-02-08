561247a78aac7cbd0dacc1617deea1a41fddc9f4
federico2001 <federicoriveroll@gmail.com>
2020-02-07 19:17:23 -0600

    Update zip feature

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from distutils.core import setup
 setup(
   name = 'OpenBlender',         # How you named your package folder (MyLib)
   packages = ['OpenBlender'],   # Chose the same as "name"
-  version = '1.6',      # Start with a small number and increase it with every change you make
+  version = '1.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'OpenBlender API Service',   # Give a short description about your library
   author = 'Federico Riveroll',                   # Type in your name
   author_email = 'federico@openblender.io',      # Type in your E-Mail
   url = 'https://www.openblender.io',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/federico2001/OpenBlender/archive/v_1_6.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/federico2001/OpenBlender/archive/v_1_7.tar.gz',    # I explain this later on
   keywords = ['OpenBlender'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
           'pandas',
           'datetime',
-          'requests'
+          'requests',
+          'base64'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
