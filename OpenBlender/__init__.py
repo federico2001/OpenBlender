42693591fbc2c9b9f0fd0642bc6f7ceb9e79cbcb
federico2001 <federicoriveroll@gmail.com>
2020-11-24 19:33:12 -0600

    LocationBlend

--- OpenBlender/__init__.py
+++ OpenBlender/__init__.py
@@ -3,3 +3,5 @@ from OpenBlender.OpenBlender import dateToUnix
 from OpenBlender.OpenBlender import unixToDate
 from OpenBlender.OpenBlender import timeBlend
 from OpenBlender.OpenBlender import searchTimeBlends
+from OpenBlender.OpenBlender import searchLocationBlends
+from OpenBlender.OpenBlender import locationBlend
